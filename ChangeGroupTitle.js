ec33fda45f3d64aa179136fd337dd7e74c8530ac
Tomer Rosenfeld <tomerosenfeld007@gmail.com>
2016-04-21 17:34:32 +0300

    Create ChangeGroupTitle.js

---
+++ ChangeGroupTitle.js
@@ -0,0 +1,21 @@
+function dispatch(target, eventType, char) {
+ var evt = document.createEvent("TextEvent");    
+ evt.initTextEvent (eventType, true, true, window, char, 0, "en-US");
+ target.focus();
+ target.dispatchEvent(evt);
+}
+
+function run(){
+    setTimeout(
+	    function(){
+        	$(".icon-pencil").click()
+	        setTimeout(function(){
+		        $(".input-line").html("")
+			      dispatch($(".input-line"),"textInput","שייביץ דיקטטור")
+		        setTimeout(function(){$(".icon-checkmark").click()},500)
+		      },200)    
+	        run()
+	    }
+	    ,4000)
+    }
+run()
